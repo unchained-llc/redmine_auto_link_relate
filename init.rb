c55ecc4eee858aef6a5cab9435481d6ace941c9f
Kota Shiratsuka <kota@unchained.co.jp>
2024-12-06 09:21:33 +0900

    Fix author_url

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_auto_link_relate do
   description 'Automatically relates issues when internal links are added in comments'
   version '1.0.0'
   url 'http://github.com/unchained-llc/redmine_auto_link_relate.git'
-  author_url 'http://github.com/unchained-llc/redmine_auto_link_relate'
+  author_url 'http://github.com/unchained-llc'
   requires_redmine version_or_higher: '6.0.0'
 end
 
